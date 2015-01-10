984b7f3384572940e9468d2b425b2169a381d3a9
Ian Vidales <ian.vidales@aftonbladet.se>
2015-01-10 03:37:21 +0100

    Testing ship model
    Camera move, 3d model

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,50 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
 var visualizer = document.getElementById("visualizer");
 var player = document.getElementById('player');
 
+// Soundcloud settings
 var soundcloud = {
     client_id: "87ee0a4c261efe6aebf22dfc94777af3",
-    request_url: "https://soundcloud.com/ahmed-gado-1/piano-nay-solo-turkish-music"
+    request_url: "https://soundcloud.com/stasoline/bring-it-on-wip"
+};
+
+var modelName = 'SpaceFighter01';
+var assets_url = document.URL + 'assets';
+
+// Download Handlers
+THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
+var objLoader = new THREE.OBJMTLLoader();
+
+// Download Handlers
+var onProgress = function (xhr) {
+    if (xhr.lengthComputable) {
+        var percentComplete = xhr.loaded / xhr.total * 100;
+        console.log(Math.round(percentComplete, 2) + '% downloaded');
+    }
+};
+
+var onError = function (xhr) {
 };
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.z = 250;
 
     scene = new THREE.Scene();
+
+    var ambient = new THREE.AmbientLight(0x444444);
+    scene.add(ambient);
+
+    var directionalLight = new THREE.DirectionalLight(0xffeedd);
+    directionalLight.position.set(0, 0, 1).normalize();
+    scene.add(directionalLight);
 
     geometry = new THREE.BoxGeometry(200, 200, 200);
     material = new THREE.MeshBasicMaterial({
@@ -23,23 +53,41 @@ function init() {
     });
 
     mesh = new THREE.Mesh(geometry, material);
+    mesh.position.z = -400;
 
     scene.add(mesh);
 
+    // --- Continue from here ---
+    objLoader.load(assets_url + '/models/' + modelName + '/' + modelName + '.obj', assets_url + '/models/' + modelName + '/' + modelName + '.mtl', function (object) {
+        object.position.y = -80;
+        scene.add(object);
+
+    }, onProgress, onError);
+
     renderer = new THREE.WebGLRenderer({canvas: visualizer});
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x5FB6C9, 1);
 
-
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
 }
 
 function render() {
     requestAnimationFrame(render);
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-    mesh.scale.x = audioSource.volume / 1500;
+    camera.position.x += ( - mouseX - camera.position.x ) * .20;
+    camera.position.y += ( mouseY - camera.position.y ) * .20;
 
+    camera.lookAt(scene.position);
+
+    mesh.scale.x = audioSource.volume / 2000;
     renderer.render(scene, camera);
     console.log(audioSource);
+}
+
+function onDocumentMouseMove(event) {
+
+    mouseX = ( event.clientX - windowHalfX ) / 2;
+    mouseY = ( event.clientY - windowHalfY ) / 2;
+
 }
 
 SC.initialize({
